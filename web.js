c09689aaf61a5e69ad55e51ad21c67ccc57cc388
bgould132 <bgould132@gmail.com>
2013-07-11 06:11:08 +0000

    Fixed web.js! And updated index.html

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFile('index.html'))
+var buf = new Buffer(fs.readFileSync('index.html'))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
